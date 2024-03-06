c0dbf3bc979f7626467b9ed88e8db2be7c633ab2
Sem-Phanun <phanunsem097@gmai.com>
2024-03-06 16:36:15 +0700

    commiting

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,11 @@
-const navbar = document.querySelector(".header-container .navbar-container .nav-list")
+const navbar = document.querySelector(".navbar-container .navbar-list")
 
 
-// document.querySelector("#menu-btn").onclick = () => {
-//     navbar.classList.add("navbar-active")
-//     navbar.classList.remove("nav-list")
-// }
-// document.querySelector("#times").onclick = ()=> {
-//     navbar.classList.remove("navbar-active")
-//     navbar.classList.add("nav-list")
-// }
+document.querySelector("#menu-btn").onclick = () => {
+    navbar.classList.add("mobile")
+    navbar.classList.remove("navbar-list")
+}
+document.querySelector("#close-btn").onclick = ()=> {
+    navbar.classList.remove("mobile")
+    navbar.classList.add("navbar-list")
+}
