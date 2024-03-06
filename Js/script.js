122775a15d3d18abb140f478f015086ecadc164d
Sem-Phanun <phanunsem097@gmai.com>
2024-03-06 15:14:53 +0700

    change ui

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,11 @@
-let navbar = document.querySelector(".header-container .navbar-container")
+const navbar = document.querySelector(".header-container .navbar-container .nav-list")
 
-document.querySelector("#menu-btn").onclick = () => {
-    navbar.classList.add("navbar-active")
-}
+
+// document.querySelector("#menu-btn").onclick = () => {
+//     navbar.classList.add("navbar-active")
+//     navbar.classList.remove("nav-list")
+// }
+// document.querySelector("#times").onclick = ()=> {
+//     navbar.classList.remove("navbar-active")
+//     navbar.classList.add("nav-list")
+// }
